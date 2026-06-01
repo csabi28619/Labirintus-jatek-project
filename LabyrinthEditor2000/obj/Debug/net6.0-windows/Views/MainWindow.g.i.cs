783e21959bcecae4b8a92440e4eb23be83814cf2
Csuborkale <mail.kiscsaba@gmail.com>
2026-06-01 17:19:01 +0200

    próba 52341

--- LabyrinthEditor2000/obj/Debug/net6.0-windows/Views/MainWindow.g.i.cs
+++ LabyrinthEditor2000/obj/Debug/net6.0-windows/Views/MainWindow.g.i.cs
@@ -477,7 +477,7 @@ namespace LabyrinthEditor.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/LabyrinthEditor;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LabyrinthEditor;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
